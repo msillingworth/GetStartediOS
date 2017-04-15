c977ae0a1a04be9fee328897a48f28a50e5ca2da
msillingworth <msillingworth@gmail.com>
2017-04-15 17:40:11 +0800

    Completed up to Persist Data

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -32,6 +32,15 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
         
+        // Set up views if editing an existing meal
+        
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the save button only if the text field has a valid Meal name.
         updateSaveButtonState()
     }
@@ -78,9 +87,19 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     // MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
+        // Depending on the style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+        
         dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside the navigation contoller.")
+        }
     }
-    
     
     // This method lets you configure a view controler before it's presented
     
